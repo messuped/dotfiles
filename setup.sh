8a4a7665e6c992fa3c4e79a1cbe5eca507a40343
Eduardo Subtil <e.subtil@campus.fct.unl.pt>
2020-03-22 23:42:21 +0000

    Removed font step as it's too convoluted, and made error only logs

--- setup.sh
+++ setup.sh
@@ -10,15 +10,15 @@ function Update {
     case $1 in
         "arch" )
             echo "Optimizing mirrors..."
-            sudo pacman-mirrors --fasttrack >> setup.log 2>&1
+            sudo pacman-mirrors --fasttrack
             echo "Updating system and installing yay..."
-            sudo pacman -Syyuu --noconfirm yay >> setup.log 2>&1
+            sudo pacman -Syyuu --noconfirm yay
         ;;
         "ubuntu" )
             echo "Updating packages..."
-            sudo apt update >> setup.log 2>&1
+            sudo apt update
             echo "Upgrading system..."
-            sudo apt upgrade -y >> setup.log 2>&1
+            sudo apt upgrade -y
         ;;
         *)
             exit 1
@@ -39,7 +39,7 @@ function Install {
                 vivaldi vivaldi-ffmpeg-codecs vivaldi-ffmpeg-codecs code discord lutris synology-drive-client
              lsd obs-studio jdk8-openjdk picard fish
             )
-            yay -Syu --noconfirm ${arch_packages[*]} >> setup.log 2>&1
+            yay -Syu --noconfirm ${arch_packages[*]}
         ;;
         "ubuntu" )
             ubuntu_packages=()
@@ -54,7 +54,13 @@ function Install {
 }
 
 set -e
-echo date > setup.log 
+
+
+date > setup.log 
+
+exec > setup.err                                                                      
+exec 2>&1
+
 echo "Detecting Distro..."
 input="$(cat /etc/os-release)" # Load OS info
 
@@ -81,19 +87,14 @@ Update $system ${packages[*]}
 echo "Done! Starting Installation process..."
 Install $system
 
-echo "Done! Installing fonts..."
-git clone https://github.com/ryanoasis/nerd-fonts.git .nerd-fonts >> setup.log 2>&1
-cd .nerd-fonts/
-./install.sh Cascadia Code >> setup.log 2>&1
-
 echo "Done! Making final configurations..."
-sudo chsh -s /usr/bin/fish $USER >> setup.log 2>&1
+sudo chsh -s /usr/bin/fish $USER
 # Install OMF
-curl -L https://get.oh-my.fish | fish >> setup.log 2>&1
-# # Setup bobthefish
-omf install bobthefish >> setup.log 2>&1
+curl -L https://get.oh-my.fish
 # Pass default config
 mv config.fish ~/.config/fish/
 
-echo "Done! Don't forget to configure alias ls='lsd' in fish!"
+echo "Done! Don't forget to configure:
+1. omf install bobthefish 
+2. alias ls='lsd' in fish!"
 set +e
