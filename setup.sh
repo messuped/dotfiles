32d2d7614d187602f60b6176037e054cdd8e19db
Eduardo Subtil <e.subtil@campus.fct.unl.pt>
2020-03-22 17:56:10 +0000

    WIP checkpoint

--- setup.sh
+++ setup.sh
@@ -1,19 +1,44 @@
 #!/bin/bash
 
 # Helper Functions
+
+# Function Update
+# Updates every OS component
+# @args:
+#   $1 - defines which OS is running (thus which package manager to use)
 function Update {
     case $1 in
         "arch" )
             # First optimize mirrors
-            #pacman-mirrors --fasttrack && pacman -Syyuu yay
-            pacman -Syu
+            #pacman-mirrors --fasttrack && pacman -Syyuu --noconfirm yay
+            pacman -Syu --noconfirm
         ;;
         "buntu" )
-            apt update && apt upgrade
+            apt update && apt upgrade -y
         ;;
         *)
             echo "Unsupported Package Manager!"
-            exit 1
+            exit 2
+        ;;
+    esac
+}
+
+# Function Install
+# Installs a given set of programs to the OS
+# @args:
+#   $1 - defines which OS is running (thus which package manager to use)
+#   $2 - array of programs to be installed supported by package manager
+function Install {
+    case $1 in
+        "arch" )
+            yay --noconfirm ${2[*]}
+        ;;
+        "buntu" )
+            apt install -y ${2[*]}
+        ;;
+        *)
+            echo "Unsupported Package Manager!"
+            exit 2
         ;;
     esac
 }
@@ -22,7 +47,7 @@ function Update {
 
 input="$(cat /etc/os-release)" # Load OS info
 
-# Get Distro by extracting input's 'NAME' field and subsequently extract its name
+# Get Distro by extracting input's 'NAME' field and subsequently extract its value
 IFS=$'"' read -ra distro <<< "IFS=$'\n' read -ra name <<< '$input'"
 
 case ${distro[1]} in
@@ -39,20 +64,31 @@ case ${distro[1]} in
 esac
 
 # Update
-Update $system
+#Update $system
 
 # Install:
+programs=(ola linda e sexy Sara)
+echo ${programs[*]} 
 
 # FISH
 # Steam
 # Vivaldi
 # Discord
 # Lutris
+# SynologyDrive
+# LSD
 
 # Install Fonts
+git clone https://github.com/ryanoasis/nerd-fonts.git
+cd nerd-fonts/
+./install.sh Cascadia Code
 
 # Setup FISH:
 # Configure as default shell
+chsh -s /usr/bin/fish $USER
 # Pass default config
+mv config.fish ~/.config/fish/
 # Install OMF
+curl -L https://get.oh-my.fish | fish
 # Setup bobthefish
+omf install bobthefish
