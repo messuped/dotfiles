e28e3e7be89bb37ddc339d4c917bac076a7cf3ab
Eduardo Subtil <e.subtil@campus.fct.unl.pt>
2020-03-24 13:52:06 +0000

    Updating config files to be linked to their directories instead of hard copying: this allows for dynamic updates to be easily done by simply git pulling

--- config_files/config.fish
+++ config_files/config.fish
@@ -1,2 +1,27 @@
-set -g PATH "$PATH:/home/messuped/flutter/bin"
+# Bob The Fish
+set -g theme_display_git yes
+set -g theme_display_git_master_branch yes
+set -g theme_display_docker_machine yes
+set -g theme_display_k8s_context yes
+set -g theme_display_hg yes
+set -g theme_display_virtualenv yes
+set -g theme_display_ruby yes
+set -g theme_display_nvm yes
+set -g theme_display_user ssh
+set -g theme_display_hostname ssh
+set -g theme_display_date no
+set -g theme_display_cmd_duration yes
+set -g theme_title_display_process yes
+set -g theme_title_display_path no
+set -g theme_title_use_abbreviated_path no
+set -g theme_date_format "+%a %H:%M"
+set -g theme_avoid_ambiguous_glyphs yes
+set -g theme_powerline_fonts no
 set -g theme_nerd_fonts yes
+set -g theme_show_exit_status yes
+set -g theme_display_jobs_verbose yes
+set -g theme_color_scheme nord
+set -g theme_project_dir_length 3
+
+# Flutter 
+set -g PATH "$PATH:/home/messuped/flutter/bin"
