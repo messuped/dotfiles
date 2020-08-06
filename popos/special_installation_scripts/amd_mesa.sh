b52cf7dde93b9fdea28b57ba6c24681ab557c2ff
Eduardo Subtil <ebzsubtil@proton.me>
2020-08-06 21:55:54 +0100

    Ensure amd_mesa install step is skipped if not on AMD system

--- popos/special_installation_scripts/amd_mesa.sh
+++ popos/special_installation_scripts/amd_mesa.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
-
-sudo add-apt-repository ppa:kisak/kisak-mesa -y
-sudo apt update
-sudo apt install libgl1-mesa-dri:i386 mesa-vulkan-drivers mesa-vulkan-drivers:i386 -y
+string=${lspci | grep -iE 'VGA|3D|video'}
+if [[ $string == *"[AMD/ATI]"* ]]; then
+  sudo add-apt-repository ppa:kisak/kisak-mesa -y
+    sudo apt update
+    sudo apt install libgl1-mesa-dri:i386 mesa-vulkan-drivers mesa-vulkan-drivers:i386 -y
+fi
